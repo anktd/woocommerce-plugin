4682222ce395f18322451abbbc47fb30519e2cb3
anktd <ankit61d@gmail.com>
2026-01-22 15:31:30 +0530

    minor improvements to crypto selection page UI

--- templates/blockonomics_crypto_options.php
+++ templates/blockonomics_crypto_options.php
@@ -6,23 +6,23 @@ $order_url = $blockonomics->get_parameterized_wc_url('page',array('show_order'=>
 ?>
 <div class="woocommerce bnomics-order-container">
   <div class="bnomics-select-container">
-    <tr>
-      <?php
-      foreach ($cryptos as $code => $crypto) {
-        $order_url = add_query_arg('crypto', $code, $order_url);
-      ?>
-        <a href="<?php echo $order_url;?>">
-          <button class="bnomics-select-options woocommerce-button button">
-            <span class="bnomics-icon-<?php echo $code;?> bnomics-rotate-<?php echo $code;?>"></span>
-            <span class="vertical-line" style="line-height: 2em;">
-              <?=__('Pay with', 'blockonomics-bitcoin-payments')?>
-              <?php echo $crypto['name'];?>
-            </span>
-          </button>
-        </a>
-      <?php 
-      }
-      ?>
-    </tr>
+    <?php
+    foreach ($cryptos as $code => $crypto) {
+      $order_url = add_query_arg('crypto', $code, $order_url);
+    ?>
+      <a href="<?php echo $order_url;?>">
+        <button class="bnomics-select-options woocommerce-button button">
+          <span class="bnomics-icon-<?php echo $code;?>"></span>
+          <span><?=__('Pay with', 'blockonomics-bitcoin-payments')?> <?php echo $crypto['name'];?></span>
+        </button>
+      </a>
+    <?php
+    }
+    ?>
+    <div class="bnomics-powered-by">
+      <a href="https://www.blockonomics.co" target="_blank" rel="noopener">
+        <?=__('Powered by', 'blockonomics-bitcoin-payments')?> <span class="bnomics-brand">Blockonomics</span>
+      </a>
+    </div>
   </div>
 </div>
